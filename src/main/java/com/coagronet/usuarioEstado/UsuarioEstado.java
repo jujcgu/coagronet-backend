324d5845c66277913a598aba7660819779b56f6e
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-09-16 16:40:25 -0500

    add meaningful user states

--- src/main/java/com/coagronet/usuarioEstado/UsuarioEstado.java
+++ src/main/java/com/coagronet/usuarioEstado/UsuarioEstado.java
@@ -26,7 +26,11 @@ public class UsuarioEstado {
     @Column(name = "use_descripcion", nullable = false, unique = true)
     private String nombre;
 
-    public static final UsuarioEstado ACTIVE = new UsuarioEstado(1L, "ACTIVE");
-    public static final UsuarioEstado PENDING_VERIFICATION = new UsuarioEstado(2L, "PENDING_VERIFICATION");
+    // Definir estados como constantes
+    public static final UsuarioEstado DESACTIVADO = new UsuarioEstado(0L, "Usuario desactivado");
+    public static final UsuarioEstado PENDIENTE_VERIFICACION = new UsuarioEstado(1L, "Usuario registrado, pero no se ha activado el email");
+    public static final UsuarioEstado ACTIVADO_SIN_INFO = new UsuarioEstado(2L, "Usuario activado, pero no ha llenado información personal y no se ha asociado a una empresa");
+    public static final UsuarioEstado ACTIVADO_SIN_EMPRESA = new UsuarioEstado(3L, "Usuario activado, ha llenado información personal, pero no se ha asociado a una empresa");
+    public static final UsuarioEstado ACTIVADO_CON_EMPRESA = new UsuarioEstado(4L, "Usuario activado, ha llenado información personal y se ha asociado a una empresa");
 }
 
