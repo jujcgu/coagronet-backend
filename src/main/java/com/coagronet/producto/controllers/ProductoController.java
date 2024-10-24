18ea9a6313acd9b790914b2826ccba7674356bae
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-10-24 13:33:38 -0500

    add Cors to ProductoController

--- src/main/java/com/coagronet/producto/controllers/ProductoController.java
+++ src/main/java/com/coagronet/producto/controllers/ProductoController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.coagronet.producto.mappers.ProductoMapper;
 import com.coagronet.producto.services.ProductoService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
 
